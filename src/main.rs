f130df3c852830d466e9e63e42bd9f9f21e1a846
AndreHSP <andrehspontes@gmail.com>
2021-07-08 14:32:10 -0300

    second pass

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-extern crate rustc_serialize;
-use rustc_serialize::json::{self, Json, ToJson};
+extern crate serde;
+extern crate serde_derive;
+extern crate serde_json;
 
-#[derive(RustcEncodable)]
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+use std::fs::{File, OpenOptions};
+
+#[derive(Serialize, Deserialize)]
 struct Dvd {
     name: String,
     year: u16,
@@ -9,17 +14,23 @@ struct Dvd {
     lenght: u16,
 }
 
-impl ToJson for Dvd {
-    fn to_json(&self) -> Json {
-        Json::String(format!(
-            "{}+{}+{}+{}i",
-            self.name, self.year, self.cast, self.lenght
-        ))
-    }
+fn json_from_str(raw: &str) -> Dvd {
+    serde_json::from_str(raw).unwrap()
 }
 
-fn converttojson(advd: &Dvd) -> String {
-    json::encode(advd).unwrap()
+fn str_from_json(dvd: &Dvd) -> String {
+    serde_json::to_string(dvd).unwrap()
+}
+
+fn dvds_to_file(f: &String, d: Dvd) {
+    let file = OpenOptions::new().append(true).open(f).unwrap();
+    serde_json::to_writer(file, &d);
+}
+
+fn dvds_from_file(f: &String) -> Dvd {
+    let file = File::open(f).unwrap();
+    let deserialized_json: Dvd = serde_json::from_reader(file).unwrap();
+    deserialized_json
 }
 
 fn main() {
@@ -29,8 +40,4 @@ fn main() {
         cast: String::from("Hugh Grant"),
         lenght: 117,
     };
-
-    let encoded = converttojson(&a);
-
-    println!("{}", encoded);
 }
